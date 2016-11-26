34e91b483e0d541bff53405cc0b199e14a222ca6
Jose7198 <jose719810@hotmail.com>
2016-11-26 14:13:15 -0500

    Update Direccion.java

--- src/Direccion.java
+++ src/Direccion.java
@@ -3,14 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package sistemahospitalario;
 
 /**
  *
- * @author USER
+ * @author JoseLuis
  */
 public class Direccion {
-    private String principal,numero;
+    private String principal;
     private String secundaria;
+    private String numero;
 
     public String getPrincipal() {
         return principal;
@@ -20,12 +22,9 @@ public class Direccion {
         this.principal = principal;
     }
 
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
+    @Override
+    public String toString() {
+        return principal + " y " + secundaria + ", " + numero;
     }
 
     public String getSecundaria() {
@@ -35,6 +34,23 @@ public class Direccion {
     public void setSecundaria(String secundaria) {
         this.secundaria = secundaria;
     }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
     
-    
+    public Direccion(){
+        this.principal="Ladron de Guevara";
+        this.secundaria="Quito";
+        this.numero="E11-253";
+    }
+    public Direccion(String principal,String secundaria,String numero){
+        this.principal=principal;
+        this.secundaria=secundaria;
+        this.numero=numero;
+    }
 }
